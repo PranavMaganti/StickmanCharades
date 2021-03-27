a5994431ddab5f39bc59b0a415a195b9f2a2b537
Jack Benham <jackbenham@indosgroup.com>
2021-03-27 22:35:44 +0000

    Made main form pretty

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -1,46 +1,79 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { useHistory } from "react-router-dom";
-import { Container, TextField, Button } from "@material-ui/core";
+import {
+  Container,
+  TextField,
+  Button,
+  Card,
+  Typography,
+} from "@material-ui/core";
 import "../index.css";
 import socket from "../socket";
+
+const useStyles = makeStyles({
+  topMargin: {
+    marginTop: 5,
+    textAlign: "center",
+  },
+  bottomMargin: {
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  bothMargin: {
+    marginTop: 5,
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  hugeTopMargin: {
+    marginTop: 200,
+    textAlign: "center",
+  },
+});
 
 export default function MainPage() {
   const [userName, setUserName] = useState("");
   const [roomCode, setRoomCode] = useState("");
   const history = useHistory();
+  const classes = useStyles();
 
   return (
-    <Container maxWidth="sm" style={{ textAlign: "center" }}>
-      <h1>Charades.me</h1>
-      <form className="input-fields">
-        <TextField
-          className="name-input"
-          defaultValue="PlayerX"
-          label="Nickname"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
+    <Container maxWidth="sm" className={classes.hugeTopMargin}>
+      <Card>
+        <Typography variant="h2">charades.me</Typography>
+        <Container maxWidth="sm" className={classes.bothMargin}>
+          <form className="input-fields">
+            <TextField
+              className={classes.bothMargin}
+              defaultValue="PlayerX"
+              label="Nickname"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
 
-        <TextField
-          className="room-input"
-          label="Room Code"
-          value={roomCode}
-          onChange={(e) => setRoomCode(e.target.value)}
-        />
+            <TextField
+              className={classes.bothMargin}
+              label="Room Code"
+              value={roomCode}
+              onChange={(e) => setRoomCode(e.target.value)}
+            />
 
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            socket.connect();
-            history.push("/play");
-          }}
-        >
-          Join Game
-        </Button>
+            <Button
+              className={classes.bothMargin}
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                socket.connect();
+                history.push("/play");
+              }}
+            >
+              Join Game
+            </Button>
 
-        <Button>Or start a new room!</Button>
-      </form>
+            <Button>Or start a new room!</Button>
+          </form>
+        </Container>
+      </Card>
     </Container>
   );
 }
