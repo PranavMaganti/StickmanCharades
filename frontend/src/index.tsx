068cfd7f3e8c2588ba53ea09a709574536a5d213
Pranav Maganti <pranav.maganti@gmail.com>
2021-03-27 12:58:29 +0000

    Add linting files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <p>Hello World</p>,
-  document.getElementById('root')
-);
+ReactDOM.render(<p>Hello World</p>, document.getElementById("root"));
