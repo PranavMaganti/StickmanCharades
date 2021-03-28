28ca7526d75dafa4bd67051460e972161ef7251e
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2021-03-28 06:59:26 +0100

    Add score to UserData

--- backend/src/UserData.ts
+++ backend/src/UserData.ts
@@ -1,9 +1,11 @@
 export default class UserData {
   userName: String;
   lastGuess: String;
+  score: number;
 
   constructor(userName: String) {
     this.userName = userName;
     this.lastGuess = "";
+    this.score = 0;
   }
 }
