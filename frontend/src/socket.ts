6db3cff440e27b07178bc846c03c3f066981ece8
Pranav Maganti <pranav.maganti@gmail.com>
2021-03-28 10:50:51 +0100

    Fix sockect

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const URL = "https://stickman-charades.herokuapp.com/";
-const socket = io(URL, { autoConnect: false }); 
+const socket = io(URL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
   console.log(event, args);
