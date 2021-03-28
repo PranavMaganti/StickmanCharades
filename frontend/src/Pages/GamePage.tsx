0d89b2313591d214bc58dc145ef46a20b3eb94a3
Jack Benham <jackbenham@indosgroup.com>
2021-03-28 01:55:51 +0000

    Made Game Page pretty <3 :)

--- frontend/src/Pages/GamePage.tsx
+++ frontend/src/Pages/GamePage.tsx
@@ -1,13 +1,38 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Card } from "@material-ui/core";
 import ChatComponent from "../ChatComponent";
 import "../index.css";
 import StickmanComponent from "../StickmanComponent";
 
+const useStyles = makeStyles({
+  topMargin: {
+    marginTop: 5,
+    textAlign: "center",
+  },
+  bottomMargin: {
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  allMargin: {
+    margin: 5,
+    textAlign: "center",
+  },
+  hugeTopMargin: {
+    marginTop: 200,
+    textAlign: "center",
+  },
+});
+
 export default function GamePage() {
+  const classes = useStyles();
+
   return (
-    <div className="game-layout">
-      <StickmanComponent />
-      <div className="chat-container">
+    <div className="flex-row">
+      <Card className={classes.allMargin} variant="outlined">
+        <StickmanComponent />
+      </Card>
+      <div>
         <ChatComponent />
       </div>
     </div>
--- frontend/src/User.tsx
+++ frontend/src/User.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import "./index.css";
-import avatar from "./Assets/user.jpg";
+import { makeStyles } from "@material-ui/core/styles";
+import { Avatar, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  padding: {
+    padding: 5,
+  },
+  leftMargin: {
+    marginLeft: 20,
+  },
+  marginAndPadding: {
+    margin: 5,
+    padding: 5,
+  },
+});
 
 export default function User() {
+  const classes = useStyles();
   return (
     <div className="flex-row">
-      <img src={avatar} className="user-img"></img>
-      <div className="flex-col">
-        <p>Nickname</p>
-        <p>Recent Chat</p>
+      <Avatar>D</Avatar>
+      <div className={[classes.leftMargin, "flex-col"].join(" ")}>
+        <Typography variant="body1" gutterBottom>
+          Dave
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          Most recent guess: Chicken!
+        </Typography>
       </div>
     </div>
   );
